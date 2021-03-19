a4efe2c76758469df3d8e2f09f8e2478de9d9084
Daniel B Kim <danielkim@Daniels-MacBook-Pro.local>
2021-03-19 09:31:49 -0400

    adds Character component to the UI

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,11 +1,13 @@
 // Write your Character component here
 import React from 'react';
 
-export default function Character(props) {
+export default function Character({ character }) {
     console.log('Character synced.');
+    // console.log(props);
+    // console.log(character);
     return (
         <div>
-            <h1>Hello!</h1>
+            <h3>{ character.name }</h3>
         </div>
     );
 };
