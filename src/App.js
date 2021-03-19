707f33e683d173462878eee52337de21b307bdce
Daniel B Kim <danielkim@Daniels-MacBook-Pro.local>
2021-03-19 12:46:21 -0400

    adds theme directory for styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 import Character from './components/Character';
+import styled from 'styled-components';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -11,6 +12,7 @@ const App = () => {
 
   // set up state
   const [ characters, setCharacters ] = useState([]);
+  const [ characterId, setCharacterId ] = useState(0);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -26,14 +28,37 @@ const App = () => {
       })
   }, []);  // without the empty array, the call keeps happening
 
+  // use styles from styled components here:
+  const StyledSection = styled.div``
+
+  const StyledTitle = styled.h1``
+
+  const StyledSubtitle = styled.h2``
+  
+  const StyledCharacter = styled.div``
+
+
+
+
+  // pass in people id prop
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <StyledSection>
+        <div>
+          <h1 className="Header">STAR WARS</h1>
+          <h2>Department of Motor Vehicles</h2>
+        </div>
+      </StyledSection>
       <div>
         {/* <Character character=/> */}
         { characters.map((char, index) => {
-          // console.log(index)
-          return <Character key={ index } character={ char }/>
+          // console.log(char.url)
+          const url = char.url
+          const people = url.slice(url.length - 2, -1);
+          // console.log(characterIdentifier);
+
+          return <Character key={ index } character={ char } people={ people }/>
         }) }
       </div>
     </div>
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,13 +1,36 @@
 // Write your Character component here
 import React from 'react';
 
-export default function Character({ character }) {
-    console.log('Character synced.');
+export default function Character({ character, people }) {
+    const peopleObj = {};
+    // STRETCH GOAL: match peopleObj to id and spit out the image
+    // console.log('Character synced.');
     // console.log(props);
-    // console.log(character);
+    console.log(character);
+    // console.log(character.key, character.url)
+    peopleObj[people] = character.name;
+    // console.log(peopleObj);
     return (
+        // { for(let i in characterUrls) {
+        //     characterUrls[character.url];
+        //     console.log(characterUrls);
+        // }; }
         <div>
+            {/* <img src={ character.url } alt='character-picture'></img> */}
             <h3>{ character.name }</h3>
+            {/* <p>Species: { character.species }</p> */}
+            {/* <p>Homeworld: { character.homeworld }</p> */}
+            <p>Height { character.height } cm</p>
+            <p>Mass: { character.mass } kg</p>
+            <p>YOB: { character.birth_year }</p>
+            <p>Eye Color: { character.eye_color }</p>
+            <p>Hair Color: { character.hair_color }</p>
+            <p>Skin Color: { character.skin_color }</p>
+            <ul>
+                {/* { character.films.map((film) => {
+                    return <li><a href='#'>{ film }</a></li>
+                })} */}
+            </ul>
         </div>
     );
 };
